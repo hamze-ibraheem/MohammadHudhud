4c6abd776932bbe1af31e7e5ea3592efc6fbe666
taskfoundation <taskfoundation.apps@gmail.com>
2023-02-10 13:12:46 +0300

    Dear sir Madam homework 5

---
+++ c++/Homework1/homework5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     cout << "Dear sir madam!\\\n";
+     cout<< "How are you?\n\n";
+     cout << "My name is \"Hamza\", nice to meet you";
+
+    return 0;
+}
