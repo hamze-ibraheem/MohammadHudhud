ef0dc6835f9c0ffc77f45de65c194576ccbcd21d
Hamza Al-Sarsour <hamze.85@live.com>
2024-05-14 05:05:21 +0300

    homework8 sum of three user inputs...

---
+++ c++/Homework1/homework8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,sumOfABC;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    sumOfABC = (int)a+(int)b+(int)c;
+    cout << sumOfABC << endl;
+    return 0;
+}
