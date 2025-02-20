bcd5218cf0eddd1cfb9802d53fd96c9916ae394a
h.alsarsour <h.alsarsour@media.gov.sa>
2025-02-21 02:26:54 +0300

    solve homework 10 operations

---
+++ c++/Homework1/homework10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, B;
+
+    cout<< "please enter the first number = "<<endl;
+    cin >> A;
+    cout << "please enter the second number = "<<endl;
+    cin >> B; 
+
+    cout<<"A + B = " << A + B << endl;
+    cout<<"A * B = " << A * B << endl;
+    cout<<"A / B = " << A / B << endl;
+    cout<<"A % B = " << A % B << endl;
+    cout<<"A - B = " << A - B << endl;
+    return 0;
+}
